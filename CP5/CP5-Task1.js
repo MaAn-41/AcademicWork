489f68a65fd379e4094c16d2f543b4eebe33e1af
MaAn-41 <f219219@cfd.nu.edu.pk>
2024-03-13 11:37:22 +0500

    CP5-Task1.html

---
+++ CP5/CP5-Task1.js
@@ -0,0 +1,11 @@
+// This is a single-line comment explaining the code
+
+let message = "Hello!"; // This line declares a variable with a comment
+
+/* This is a multi-line comment
+  used to document a function */
+function greet() {
+  console.log(message);
+}
+
+greet(); // Call the greet function
